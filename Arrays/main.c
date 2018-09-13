b2de422b6b364ff59c657b5116b23aff61f6b7af
german2017 <scarafiloutn@gmail.com>
2018-09-13 11:46:32 -0300

    Add files via upload

--- Arrays/main.c
+++ Arrays/main.c
@@ -4,16 +4,42 @@
 #include <ctype.h>
 #define T 3
 
+a) Cargar los datos (carga aleatoria)
+b) modificar: Se ingresa el legajo, lo busca y si lo encuentra modifica
+  la nota 1
+c) Informes
+1- los alumnos con promedio 6 o mas
+2- El/Los alumnos con maximo promedio
+3- La/s alumna/s con minimo promedio
+4- Cantidad de alumnos desaprobados (Se aprueba con 6 o mas en el promedio)
+5- Cantidad de alumnos aprobados
+6- Procentaje de aprobados y desaprobados
+7- Mostrar Los alumnos que se llamen Juan
+8- Ordenar los datos alfabeticamente por nombre
+9- Ordenar por nombre y ante igualdad de nombre por promedio
+10- Mostrar Los alumnos cuyo nombre comience con P
+11- Ordenar alumnos por sexo
+12- Realizar un grafico que muestre:
+    Alumnos entre 15-18 (inclusive)
+                  19 y 25 (inclusive)
+                  26 o mas
+
+
+
 float calcularPromedio(int,int,int);
 int cargarAlumnos(char[][50], int[],int[],int[],float[], int);
 int mostrarAlumnos(char[][50], int[],int[],int[],float[], int);
 int main()
 {
-    char nombreCompleto[T][50];
+    legajo
+    char nombre[T][50];
     int nota1[T];
     int nota2[T];
     int nota3[T];
     float promedio[T];
+    char email[T][]
+    char sexo;
+    int Edad
     int i;
 
     cargarAlumnos(nombreCompleto,nota1,nota2,nota3, promedio, T);
